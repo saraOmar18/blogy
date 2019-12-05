f2bc5a6ee42d10ce1b0a19edb2e46987cfbfa043
saraOmar18 <sezla18@gmail.com>
2019-12-05 16:29:04 +0300

    delete function

--- app/routes/articles.js
+++ app/routes/articles.js
@@ -1,37 +1,89 @@
-const express =require('express'); 
-const router =express.Router(); 
-const Article = require ('../models/article');
-
-
-//GET ALL ARTICLES 
-router.get('/articles',(req,res)=> {
-    res.json({message:'welcome to articles'});
-}); 
-
-// SHOW ARTICLE BY ID --GET 
-router.get('/articles', (req, res) => {
-    Article.find({}, (error, articles) => {
-        if (!error) { 
-            res.status(200).json({ articles: articles });
-        } else {
-            res.status(500).json({ error: error})
-        }
-    });
+// Require necessary NPM Packages
+const express = require('express');
+// Require Mongoose Model for Article
+const Article = require('../models/article');
+// Instantiate a Router (mini app that only handles routes)
+const router = express.Router();
+/**
+ * Action:      INDEX
+ * Method:      GET
+ * URI:         /api/articles
+ * Description: Get All Articles
+ */
+router.get('/api/articles', (req, res) => {
+  Article.find()
+  // Return all Articles as an Array
+  .then((articles) => {
+    res.status(200).json({ articles: articles });
+  })
+  // Catch any errors that might occur
+  .catch((error) => {
+    res.status(500).json({ error: error });
+  });
 });
+/**
+* Action:       SHOW
+* Method:       GET
+* URI:          /api/articles/5d664b8b68b4f5092aba18e9
+* Description:  Get An Article by Article ID
+*/
+/**
+ * Action:      CREATE
+ * Method:      POST
+ * URI:         /api/articles
+ * Description: Create a new Article
+*/
+router.post('/api/articles', (req, res) => {
+  Article.create(req.body.article)
+  // On a successful `create` action, respond with 201
+  // HTTP status and the content of the new article.
+  .then((newArticle) => {
+    res.status(201).json({ article: newArticle });
+  })
+  // Catch any errors that might occur
+  .catch((error) => {
+    res.status(500).json({ error: error });
+  });
+});
+/**
+ * Action:      UPDATE
+ * Method:      PATCH
+* URI:          /api/articles/5d664b8b68b4f5092aba18e9
+* Description:  Update An Article by Article ID
+ */
 
-// CREATE NEW ARTICLE --POST
-// router.post('/articles', (req, res) => {
-//     res.json(req.body)
-// });
 
 
-// UPDATE ARTICLE BY ID --PATCH 
-
-
-
-// DELETE ARTICLE BY ID --DESTROY 
-
-
-
-
-module.exports=router;
+/**
+ * Action:      DESTROY
+ * Method:      DELETE
+* URI:          /api/articles/5d664b8b68b4f5092aba18e9
+* Description: Delete An Article by Article ID
+ */
+router.delete('/api/articles/:id', (req, res) => {
+    Article.findByid(req.params.id)
+      .then((article) => {
+        if(article) {
+          // Pass the result of Mongoose's `.delete` method to the next `.then`
+          return article.remove();
+        } else {
+          // If we couldn't find a document with the matching ID
+          res.status(404).json({
+            error: {
+              name: 'DocumentNotFoundError',
+              message: 'The provided ID doesn\'t match any documents'
+            }
+          });
+        }
+      })
+      .then(() => {
+        // If the deletion succeeded, return 204 and no JSON
+        res.status(204).end();
+      })
+      // Catch any errors that might occur
+      .catch((error) => {
+        res.status(500).json({ error: error });
+      });
+  });
+  // Export the Router so we can use it in the server.js file
+  module.exports = router;
--- server.js
+++ server.js
@@ -1,25 +1,31 @@
-const express =require('express'); 
-const mongoose=require('mongoose'); 
-
-const indexRouter=require('./app/routes/index'); 
-const articlesRouter=require('./app/routes/articles'); 
-
-
-const db = require('./config/db')
-mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true}, ()=> {
-    console.log("Connected to MongoDB")
+// Require necessary NPM Packages
+const express = require('express');
+const mongoose = require('mongoose');
+// Require Route Files
+const indexRouter = require('./app/routes/index');
+const articlesRouter = require('./app/routes/articles');
+// Require DB Configuration File
+const db = require('./config/db');
+// Establish Database Connection
+mongoose.connect(db, { useNewUrlParser: true });
+mongoose.connection.once('open', () => {
+  console.log('Connected to Mongo');
+})
+// Instantiate Express Application Object
+const app = express();
+// Define PORT for the API to run on
+const port = process.env.PORT || 5000;
+/*** Middleware ***/
+// Add `bodyParser` middleware which will parse JSON requests into
+// JS objects before they reach the route files.
+//
+// The method `.use` sets up middleware for the Express application
+app.use(express.json());
+/*** Routes ***/
+// Mount imported Routers
+app.use(indexRouter);
+app.use(articlesRouter);
+// Start the server to listen for requests on a given port
+app.listen(port, () => {
+  console.log(`blogy is listening on port ${port}`);
 });
-
-const app=express(); 
-
-app.use(indexRouter); 
-app.use(articlesRouter); 
-
-
-
-
-const port=process.env.PORT || 5000; 
-app.listen(port,()=> {
-console.log(`blogy is listening on port ${port}`); 
-});
-
